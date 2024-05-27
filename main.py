2b3fcf62c278a25e026696d17019b9c4872623c3
jhon <jhonatanrian.jr@gmail.com>
2024-05-27 19:46:51 -0300

    interagindo melhor

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ idade = 18
 nome = input("Qual seu nome? ")
 
 print(f"Olá {nome}, como vai?")
+
+confirmar = input(f"Você tem {idade} anos? ")
+
+if confirmar == "s":
+    print("Que legal!")
+else:
+    print("Tchau")
