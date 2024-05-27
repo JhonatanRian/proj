91a817f5e37f17b4f0b5eb72a34b0a8923e82529
JhonatanRian <jhonatanrian.jr@gmail.com>
2024-05-27 19:24:19 -0300

    verificando idade

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("olá mundo")
 
 print("Estou estudando python")
+
+idade = int(input("digite sua idade: "))
+
+if idade >= 18:
+    print("voce pode entrar")
+else:
+    print("voce nao pode entrar")
